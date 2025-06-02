cd83231ee2648fd92ebea849fde25207993e6df3
Jonathan Boetsch <jonathan.boetsch@gmail.com>
2025-06-02 10:43:17 +0200

    Feat: add boolean isAlive to PlacedShip, add constructor + setters/getters

--- src/main/java/se/boetsch/Battleship/domain/PlacedShip.java
+++ src/main/java/se/boetsch/Battleship/domain/PlacedShip.java
@@ -20,6 +20,58 @@ public class PlacedShip {
     private ShipCoordinates shipCoordinates;
     private ShipOrientation orientation;
     private Player player;
+    private boolean isAlive = true;
+
+    public PlacedShip(ShipModel shipModel, ShipCoordinates shipCoordinates, ShipOrientation orientation, Player player) {
+        this.shipModel = shipModel;
+        this.shipCoordinates = shipCoordinates;
+        this.orientation = orientation;
+        this.player = player;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public ShipModel getShipModel() {
+        return shipModel;
+    }
+
+    public void setShipModel(ShipModel shipModel) {
+        this.shipModel = shipModel;
+    }
+
+    public ShipCoordinates getShipCoordinates() {
+        return shipCoordinates;
+    }
+
+    public void setShipCoordinates(ShipCoordinates shipCoordinates) {
+        this.shipCoordinates = shipCoordinates;
+    }
+
+    public ShipOrientation getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(ShipOrientation orientation) {
+        this.orientation = orientation;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 
     // TODO: add sessionUUID;
 
