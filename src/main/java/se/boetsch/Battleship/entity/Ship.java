835a435162bf4ffea707bf4f124cf84c9ae81b2d
Jonathan Boetsch <jonathan.boetsch@gmail.com>
2025-06-02 09:50:53 +0200

    Feat: create Ship class

---
+++ src/main/java/se/boetsch/Battleship/entity/Ship.java
@@ -0,0 +1,45 @@
+package se.boetsch.Battleship.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Ship implements Serializable {
+
+    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    private String name;
+
+    @JsonIgnore
+    private int size;
+
+    public Ship(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+}
