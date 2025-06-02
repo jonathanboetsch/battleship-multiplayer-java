39f15354b0c2181650f29a6dadc3670404ad7905
Jonathan Boetsch <jonathan.boetsch@gmail.com>
2025-06-02 10:19:27 +0200

    Chore: remove unnecessary imports

--- src/main/java/se/boetsch/Battleship/entity/Player.java
+++ src/main/java/se/boetsch/Battleship/entity/Player.java
@@ -2,11 +2,6 @@ package se.boetsch.Battleship.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import se.boetsch.Battleship.domain.PlacedShip;
-import se.boetsch.Battleship.service.BattleMap;
-
-import java.util.List;
-import java.util.Set;
 
 @Entity
 public class Player {
